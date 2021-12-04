9678c1b482d4b5fc7f2123701d39cab5a12a2a18
Ievgen Shakin <shakinzhenya@gmail.com>
2021-12-04 02:11:25 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const formEl = document.querySelector('.login-form');
+const inputEl = document.querySelectorAll('input');
+
+formEl.addEventListener('submit', onSendForm);
+
+function onSendForm(event) {
+  event.preventDefault();
+  
+  if (event.currentTarget.value === '') {
+    alert('All fields must be filled!');
+  }
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+  console.log('Email:', email.value, 'Password:', password.value);
+  formEl.reset();
+};
+
 
